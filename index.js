71ae307e8031a6c45afea1b5e5b5fa48179bcebd
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-08-23 13:08:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 12;
+let num4 = 8;
 
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let  mod = num3 % num4;
+
+let  max = Math.max(1, 2, 5, 8, 10, 12, 15, 20);
 // Write your code here
